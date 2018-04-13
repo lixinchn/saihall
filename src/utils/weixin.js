b04edc4f29069ec32b9c37e09634d88f6e5feaff
lixin <lixinit@gmail.com>
2018-04-13 16:11:33 +0800

    debug

--- src/utils/weixin.js
+++ src/utils/weixin.js
@@ -42,7 +42,7 @@ export default {
 
   config(noncestr, timestamp, url, ticket, sig, self, dispatch, shareUrl) {
     wx.config({
-      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: 'wxe15979ce3adafcd9', // 必填，企业号的唯一标识，此处填写企业号corpid
       timestamp: timestamp, // 必填，生成签名的时间戳
       nonceStr: noncestr, // 必填，生成签名的随机串
